31492b487305d68c79c10c09d1aafe27cb132daf
Nguyen Hoang <hoangndhhe180790@fpt.edu.vn>
2026-03-05 11:03:41 +0700

    fix logout

--- EbayCloneWeb/Pages/Account/Logout.cshtml.cs
+++ EbayCloneWeb/Pages/Account/Logout.cshtml.cs
@@ -5,9 +5,11 @@ namespace EbayCloneWeb.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        public IActionResult OnGet()
+        public IActionResult OnPost()
         {
             HttpContext.Session.Clear();
+
+            TempData["Success"] = "You have been logged out.";
 
             return RedirectToPage("/Account/Login");
         }
